7749c8338f9cb40ca2b6a2881aacde2831dca296
Albert Ho <albertcyi.ho@gmail.com>
2022-02-10 09:18:28 -0800

    Update function to query lightbnb database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -79,7 +79,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `
+      SELECT reservations.*,
+        properties.*,
+        AVG(rating) AS average_rating
+      FROM reservations
+        INNER JOIN properties ON reservations.property_id = properties.id
+        INNER JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+        AND end_date < now()::date
+      GROUP BY reservations.id,
+        properties.id
+      ORDER BY start_date
+      LIMIT $2;
+    `,
+      [guest_id, limit]
+    )
+    .then(result => result.rows)
+    .catch(() => null);
 };
 exports.getAllReservations = getAllReservations;
 
